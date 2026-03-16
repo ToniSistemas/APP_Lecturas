c67efcbaea445643df8f935ec06d9b51919eaeb9
ToniSistemas <tiglesias@sistemasdixitais.com>
2026-03-16 21:39:41 +0100

    feat: show counter_photo in reading form view (top-right), use camera_image widget in photo list

--- water_meter_reading/__manifest__.py
+++ water_meter_reading/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Water Meter Readings',
-    'version': '18.0.1.7.0',
+    'version': '18.0.1.8.0',
     'summary': 'Registro de lecturas de contadores de agua',
     'category': 'Tools',
     'author': 'Toni',
