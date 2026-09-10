aef9bbd85ae8c0c4af3652f62b9e67a713396da7
ToniSistemas <tiglesias@sistemasdixitais.com>
2026-09-10 20:53:01 +0200

    feat: enable reading photo zoom

--- water_meter_reading/__manifest__.py
+++ water_meter_reading/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name': 'Water Meter Readings',
-    'version': '19.0.11.8.0',
+    'version': '19.0.11.9.0',
     'summary': 'Registro de lecturas de contadores de agua',
     'category': 'Tools',
     'author': 'Toni',
