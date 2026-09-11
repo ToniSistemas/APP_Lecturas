b727334b563d8688b2c366ed6a2bcc77e248d426
ToniSistemas <tiglesias@sistemasdixitais.com>
2026-09-11 17:39:53 +0200

    feat: open water readings for water users

--- water_meter_reading/models/res_users.py
+++ water_meter_reading/models/res_users.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import api, fields, models
 
 
 class ResUsers(models.Model):
@@ -8,3 +8,33 @@ class ResUsers(models.Model):
         string='Puede modificar lectura anterior',
         default=False,
     )
+
+    def _set_water_home_action(self):
+        action = self.env.ref(
+            'water_meter_reading.action_water_period',
+            raise_if_not_found=False,
+        )
+        if not action:
+            return
+        water_group = self.env.ref(
+            'water_meter_reading.group_water_user',
+            raise_if_not_found=False,
+        )
+        if not water_group:
+            return
+        users = self.filtered(lambda user: water_group in user.all_group_ids)
+        users.filtered(lambda user: user.action_id != action).sudo().write({
+            'action_id': action.id,
+        })
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        users = super().create(vals_list)
+        users._set_water_home_action()
+        return users
+
+    def write(self, vals):
+        result = super().write(vals)
+        if 'group_ids' in vals:
+            self._set_water_home_action()
+        return result
